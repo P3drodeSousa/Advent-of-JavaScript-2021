906ad8336bbb53000f9ac40f4d62f2316d5c7bb7
unknown <pedro_de_sousa@outlook.com>
2022-11-09 09:49:57 +0100

    Initial commit

--- app.js
+++ app.js
@@ -1,78 +1,44 @@
-const minInput = document.querySelector(".minutes input");
-const secInput = document.querySelector(".seconds input");
-const btnStart = document.querySelector("button.start");
-const ring = document.querySelector(".ring");
-const inputs = [minInput, secInput];
-
-let min = minInput.value;
-let sec = secInput.value;
-
-let isPaused = true;
-let nIntervId;
-
-const renderBtnTxt = () => {
-  return (btnStart.textContent = isPaused ? "Start" : "Pause");
-};
-
-const pause = () => {
-  clearInterval(nIntervId);
-};
-
-const resetTimer = () => {
-  ring.classList.remove("ending");
-  minInput.value = "15";
-  secInput.value = "00";
-  renderBtnTxt();
-};
-
-const startTimer = () => {
-  min = minInput.value;
-  sec = secInput.value;
-  isPaused = !isPaused;
-
-  renderBtnTxt();
-
-  return isPaused ? pause() : myTimer();
-};
-
-const myTimer = () => {
-  nIntervId = setInterval(() => {
-    if (!sec) {
-      isPaused = true;
-      ring.classList.add("ending");
-      pause();
-      return setTimeout(function () {
-        alert("🚨Timer's up !!");
-        resetTimer();
-      }, 10);
+const menuItems = [
+    {
+        name: 'French Fries with Ketchup',
+        price: 223,
+        image: 'plate__french-fries.png',
+        alt: 'French Fries',
+        count: 0
+    },
+    {
+        name: 'Salmon and Vegetables',
+        price: 512,
+        image: 'plate__salmon-vegetables.png',
+        alt: 'Salmon and Vegetables',
+        count: 0
+    },
+    {
+        name: 'Spaghetti Meat Sauce',
+        price: 782,
+        image: 'plate__spaghetti-meat-sauce.png',
+        alt: 'Spaghetti with Meat Sauce',
+        count: 0
+    },
+    {
+        name: 'Bacon, Eggs, and Toast',
+        price: 599,
+        image: 'plate__bacon-eggs.png',
+        alt: 'Bacon, Eggs, and Toast',
+        count: 0
+    },
+    {
+        name: 'Chicken Salad with Parmesan',
+        price: 698,
+        image: 'plate__chicken-salad.png',
+        alt: 'Chicken Salad with Parmesan',
+        count: 0
+    },
+    {
+        name: 'Fish Sticks and Fries',
+        price: 634,
+        image: 'plate__fish-sticks-fries.png',
+        alt: 'Fish Sticks and Fries',
+        count: 0
     }
-
-    if (sec == 00) {
-      min -= 1;
-      sec = 59;
-      minInput.value = min < 10 ? "0" + min : min;
-      secInput.value = sec < 10 ? "0" + sec : sec;
-    } else {
-      sec -= 1;
-      secInput.value = sec < 10 ? "0" + sec : sec;
-    }
-  }, 1000);
-};
-
-// EVENT LISTENERS
-btnStart.addEventListener("click", startTimer);
-
-inputs.forEach((el) => {
-  el.addEventListener("change", (e) => {
-    el.setAttribute("value", e.target.value);
-  });
-});
-
-document.querySelector(".settings").addEventListener("click", (e) => {
-  isPaused = true;
-  pause();
-  renderBtnTxt();
-  inputs.forEach((el) => {
-    el.toggleAttribute("disabled");
-  });
-});
+]

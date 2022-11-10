32a69beaa6fbd19994eeef5cfe7c81bbb848bc07
unknown <pedro_de_sousa@outlook.com>
2022-11-10 09:50:36 +0100

    Initial Commit

--- app.js
+++
@@ -1,6 +0,0 @@
-const rangeInput = document.getElementById("priceRange");
-
-rangeInput.addEventListener("mousemove", (e) => {
-  document.querySelector("div.amount span.dollars").textContent =
-    e.target.value / 100;
-});

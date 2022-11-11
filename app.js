21a97818ce98e72830f68d8855062c9d0971a443
unknown <pedro_de_sousa@outlook.com>
2022-11-11 14:40:07 +0100

    Initial commit

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-const questions = document.querySelectorAll("ul li a");
+const userChoices = document.querySelectorAll("li > button");
 
-let prec;
-
-const toggleClass = (target) => {
-  target.classList.toggle("expand");
-};
-
-const changeIcon = (target) => {
-  target.classList.add("visible");
-};
-
-questions.forEach((el) => {
+let userChoice;
+// E.H
+userChoices.forEach((el) => {
   el.addEventListener("click", (e) => {
-    if (prec == e.target.parentNode) {
-      toggleClass(prec);
-      prec = null;
-      return;
-    }
+    e.preventDefault();
+    userChoice = e.target.textContent.trim();
+    window.userChoice = userChoice;
 
-    if (!prec) {
-      prec = e.target.parentNode;
-    } else {
-      toggleClass(prec);
-      prec = e.target.parentNode;
-    }
-    toggleClass(e.target.parentNode);
-    changeIcon(e.target.children[0]);
+    window.location = "http://127.0.0.1:5500/winner.html";
   });
 });

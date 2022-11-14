a2fb143aabcdbe8e8e6926f63f2f2c8ea8487788
unknown <pedro_de_sousa@outlook.com>
2022-11-14 12:28:44 +0100

    Initial commit

--- app.js
+++ app.js
@@ -1,29 +1,7 @@
-const titles = document.querySelectorAll("h3");
-const contents = document.querySelectorAll("ul li a");
+const passwordLenght = document.querySelector("div.field>input#length");
+let length = document.querySelector("div.field>input#length").value;
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      console.log(entry.isIntersecting);
-      console.log(
-        titles[
-          Array.from(contents, (content) => content.textContent).indexOf(
-            entry.target.textContent
-          )
-        ]
-      );
-      /*    titles[
-        Array.from(contents, (content) => content.textContent).indexOf(
-          entry.target.textContent
-        )
-      ].classList.toggle("selected", entry.isIntersecting); */
-    });
-  },
-  {
-    threshold: 0.5,
-  }
-);
-
-titles.forEach((title) => {
-  observer.observe(title);
+//E:L
+passwordLenght.addEventListener("change", (e) => {
+  console.log(e.target.value);
 });
